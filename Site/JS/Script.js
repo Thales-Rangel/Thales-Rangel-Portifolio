b6453a64d5443b79d063241919a0279ef4c8393c
Thales Rangel <thalesran2006@gmail.com>
2024-05-06 17:13:59 -0300

    Validador de email (Segunda tentativa)
    Após uma leve pesquisada, creio que agora será possível validar o email.

--- Site/JS/Script.js
+++ Site/JS/Script.js
@@ -7,27 +7,30 @@ const descricao = document.querySelector('#description');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    if(nome.value.trim() === ""){
+    if (nome.value.trim() === "") {
         alert("O nome está vazio");
         return;
     }
 
-    if(email.value.trim() === "" || isEmailValid(email.value)){
-        alert("O email está vazio");
+    if (email.value.trim() === "" || isEmailValid(email.value)) {
         return;
     }
 
-//    form.subimit();
+    alert("Enviado");
+    //    form.subimit();
 });
 
-function isEmailValid(email){
+function isEmailValid(email) {
+    usuario = email.value.substring(0, email.value.indexOf("@"));
+    dominio = email.value.substring(email.value.indexOf("@") + 1, email.value.length);
 
-    const emailRegex = new RegExp(
-        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
-    );
-
-    if(emailRegex.test(email)){
+    if ((usuario.length >= 1) && (dominio.length >= 3) &&
+        (usuario.search("@") == -1) && (dominio.search("@") == -1) &&
+        (usuario.search(" ") == -1) && (dominio.search(" ") == -1) &&
+        (dominio.search(".") != -1) &&
+        (dominio.indexOf(".") >= 1) && (dominio.lastIndexOf(".") < dominio.length - 1)) {
         return true;
+    } else {
+        return false;
     }
-    return false;
 }
